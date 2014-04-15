8880117f4dc3eab35bde15629eb6e0d932afd296
Dagmar Timler <dagmar.timler@gmail.com>
2014-04-15 14:18:08 +0200

    INTERNAL-415 : fixing backdoor in RESTClient for MOSAIC

--- webapp/src/main/groovy/org/celllife/mosaic/interfaces/service/ReportController.groovy
+++ webapp/src/main/groovy/org/celllife/mosaic/interfaces/service/ReportController.groovy
@@ -1,13 +1,19 @@
 package org.celllife.mosaic.interfaces.service
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
+import org.celllife.mosaic.framework.restclient.RESTClient
+
 @Controller
 class ReportController {
+
+	@Autowired
+	RESTClient client;
 
     @Value('${external.base.url}')
     def String externalBaseUrl;
@@ -20,7 +26,7 @@ class ReportController {
     @RequestMapping(value="index", method = RequestMethod.GET)
     def getReports(Model model) {
 
-        def reports = org.celllife.mosaic.framework.restclient.RESTClient.get("${externalBaseUrl}/service/reports")
+        def reports = client.get("${externalBaseUrl}/service/reports")
         model.put("reports", reports)
         return "index";
 
